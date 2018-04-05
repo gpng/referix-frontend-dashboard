b80bb61ac27c8892caa19d5dc901172cdf5f72f4
Gerald Png <geraldpng@gmail.com>
2018-04-05 08:17:19 +0800

    added api route for production

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
+// set axios base config if in production
+import axiosDefaults from 'axios/lib/defaults';
+if (process.env.NODE_ENV === 'production') {
+  axiosDefaults.baseURL = 'http://referix-api.herokuapp.com';
+}
+
 // manage all actions exports
 export * from 'actions/authentication';
 export * from 'actions/responsive';
