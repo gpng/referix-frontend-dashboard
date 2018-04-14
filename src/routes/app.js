573dd1c49d815195c3bfe3ac15e029b74819b12a
Gerald Png <geraldpng@gmail.com>
2018-04-14 13:51:55 +0800

    clean up unused views

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,26 +1,11 @@
 import DashboardPage from 'views/Dashboard/Dashboard.js';
 import UserProfile from 'views/UserProfile/UserProfile.js';
-// import TableList from 'views/TableList/TableList.js';
-// import Typography from 'views/Typography/Typography.js';
-// import Icons from 'views/Icons/Icons.js';
-// import Maps from 'views/Maps/Maps.js';
-// import NotificationsPage from 'views/Notifications/Notifications.js';
 import UserManagement from 'views/UserManagement/UserManagement';
 import JobManagement from 'views/JobManagement/JobManagement';
 import JobSearch from 'views/JobSearch/JobSearch';
 import JobsApplied from 'views/JobsApplied/JobsApplied';
 
-import {
-  Dashboard,
-  Person,
-  // ContentPaste,
-  // LibraryBooks,
-  // BubbleChart,
-  // LocationOn,
-  // Notifications,
-  Work,
-  Search
-} from 'material-ui-icons';
+import { Dashboard, Person, Work, Search } from 'material-ui-icons';
 
 const appRoutes = [
   {
@@ -71,46 +56,6 @@ const appRoutes = [
     component: UserProfile,
     access: 6
   },
-  // {
-  //   path: '/table',
-  //   sidebarName: 'Table List',
-  //   navbarName: 'Table List',
-  //   icon: ContentPaste,
-  //   component: TableList,
-  //   access: 7
-  // },
-  // {
-  //   path: '/typography',
-  //   sidebarName: 'Typography',
-  //   navbarName: 'Typography',
-  //   icon: LibraryBooks,
-  //   component: Typography,
-  //   access: 7
-  // },
-  // {
-  //   path: '/icons',
-  //   sidebarName: 'Icons',
-  //   navbarName: 'Icons',
-  //   icon: BubbleChart,
-  //   component: Icons,
-  //   access: 7
-  // },
-  // {
-  //   path: '/maps',
-  //   sidebarName: 'Maps',
-  //   navbarName: 'Map',
-  //   icon: LocationOn,
-  //   component: Maps,
-  //   access: 7
-  // },
-  // {
-  //   path: '/notifications',
-  //   sidebarName: 'Notifications',
-  //   navbarName: 'Notifications',
-  //   icon: Notifications,
-  //   component: NotificationsPage,
-  //   access: 7
-  // },
   { redirect: true, path: '/', to: '/dashboard', navbarName: 'Redirect' }
 ];
 
